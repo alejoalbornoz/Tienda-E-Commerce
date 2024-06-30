bbb42336b4c9d6a184e290ca45bbfc4bc99310c0
alejoalbornoz <alejoalbornoz912@gmail.com>
2024-06-30 19:16:08 -0300

    Codigo de Promoción

---
+++ Desktop/TiendaOnline/TiendaOnline/TiendaOnline/urls.py
@@ -0,0 +1,29 @@
+
+from django.contrib import admin
+from django.urls import path
+from . import view
+from products.views import ProductListViews
+from django.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", ProductListViews.as_view(), name="index"),
+    path("usuarios/login", view.login_view, name="login"),
+    path("usuarios/logout", view.logout_view, name="logout"),
+    path("usuarios/register", view.register, name="register"),
+    path("productos/", include("products.urls")),
+    path("carrito/", include("carts.urls")),
+    path("orden/", include("orders.urls")),
+    path("direcciones/", include("shipping_addresses.urls")),
+    path("codigos/", include("promo_codes.urls")) 
+
+
+
+]
+
+
+#esto permite poner imagenes en ADMIN
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ Desktop/TiendaOnline/TiendaOnline/products/views.py
@@ -0,0 +1,48 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render
+
+from django.views.generic.list import ListView
+from .models import Product
+from django.views.generic.detail import DetailView
+from django.db.models import Q
+
+
+# Create your views here.
+
+class ProductListViews(ListView):
+    template_name = "index.html"
+    queryset = Product.objects.all().order_by("-id")
+
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context ["message"] = "Listado de Productos"
+        return context 
+    
+
+class ProductDetailView(DetailView):
+    model = Product 
+    template_name = "products/product.html"
+
+
+class ProductSearchListView(ListView):
+    
+    template_name = "products/search.html"
+
+    def get_queryset(self):
+        filters = Q(title__icontains=self.query()) | Q(category__title__icontains=self.query())
+        return Product.objects.filter(filters)
+    
+
+    def query(self):
+        return self.request.GET.get("q")
+    
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context ["query"] = self.query()
+        context["count"] = context["product_list"].count()
+        
+        return context 
